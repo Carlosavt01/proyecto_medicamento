0965f69c48e849200cbaa1104638a0b5ade62773
Carlosavt01 <64100494+Carlosavt01@users.noreply.github.com>
2022-12-01 20:03:11 -0800

    Updated procfile

--- Frontend/material-dashboard-angular2/server.js
+++ Frontend/material-dashboard-angular2/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('*', (req, res) =>
+app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: __dirname + '/dist'})
     // {root: 'dist/'}
     // ),
