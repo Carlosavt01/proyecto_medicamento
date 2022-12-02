88f942aab906f7641448bc045976a251434eb063
Carlosavt01 <64100494+Carlosavt01@users.noreply.github.com>
2022-12-01 19:37:18 -0800

    Added correct route to procfile, serving static files from express server @ /dist - required command ng build to create build output

--- Frontend/material-dashboard-angular2/server.js
+++ Frontend/material-dashboard-angular2/server.js
@@ -3,13 +3,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+const PORT = process.env.PORT || 3000
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/material-dashboard-angular2'));
+app.use(express.static('./dist/'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-);
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/'}),
+// );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(PORT, () => {
+    console.log(`App listening on port: ${PORT}`)
+});
