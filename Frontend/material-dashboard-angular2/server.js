682eff57b6ad69c9a3d9000465a3423ec7a50282
Carlosavt01 <64100494+Carlosavt01@users.noreply.github.com>
2022-12-01 19:43:57 -0800

    Updated procfile

--- Frontend/material-dashboard-angular2/server.js
+++ Frontend/material-dashboard-angular2/server.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 3000
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/'));
 
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/'}),
-// );
+app.get('*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(PORT, () => {
